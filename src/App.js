0b74fdaf6e49361ffbba82203e96f350a733f12c
Ben-Worrall <benworrallpc@gmail.com>
2024-07-22 16:38:36 +0200

    Front End

--- src/App.js
+++ src/App.js
@@ -4,20 +4,23 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
+      <div id='Leaderboard'>
+        <button id='LeaderboardButton'>Leaderboard</button>
+      </div>
+
+
+      <div id='StartDiv'>
+        <button id='StartButton'>
+          Start
+        </button>
+
+      </div>
+
+
+
+
     </div>
   );
 }
